def7779f33904041e9bd84433b069c3a9b33ab46
Anju Vijay <anjuvijaytk@gmail.com>
2025-07-18 10:09:03 +0400

    Form data submission in AddMeal page is successfully implemented.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import profile_icon from '../assets/profile_icon.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='flex items-center justify-between bg-yellow-600 p-5'>
-        <h1 className='font-bold text-2xl text-gray-800'>Nimz Meal Planner</h1>
+        <Link to='/' ><h1 className='font-bold text-2xl text-gray-800'>Nimz Meal Planner</h1></Link> 
         <img className='w-5'src={profile_icon} alt='Profile_icon'/>
     </div>
   )
